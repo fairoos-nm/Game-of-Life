1970758781779701f00c0c8c04c181fb3a73cae1
fairoos <fairoosnm123@gmail.com>
2019-02-16 12:31:56 +0530

    Added test file for Game_of_life (test_game_of_life).
    RED: Added test for matrix function.
    
    which test that when we assign the row, column and element of matrix, is it provide corespinding matrix.

---
+++ test_game_of_life.py
@@ -0,0 +1,8 @@
+#test_game_of_life
+
+import Game_of_life
+
+def test_metrix():
+    assert Game_of_life.metrix(0, 5, 5) == [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+    
+    
