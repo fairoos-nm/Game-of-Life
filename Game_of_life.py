8af1ea6e21e3613a71a95fb6b7749b4bfc189799
fairoos <fairoosnm123@gmail.com>
2019-02-16 19:11:58 +0530

    GREEN: Added initial_matrix function
    It makes a initial matrix
    some changes are made in test_initial_matrix

--- Game_of_life.py
+++ Game_of_life.py
@@ -3,3 +3,15 @@
 def matrix(element, row, column):
     matrix_out = [[element] * row for i in range(column)]
     return matrix_out
+
+a =1
+matrix1 = [[0, 0, 0],[1, 1, 1], [0, 0, 0]]
+
+def initial_matrix(a, matrix1):
+    for sub_list in matrix1:
+        if matrix1[1] == sub_list:
+            for i in sub_list:
+                sub_list[i] = 1
+                return matrix1
+
+        
--- test_game_of_life.py
+++ test_game_of_life.py
@@ -6,9 +6,10 @@ def test_matrix():
     assert Game_of_life.matrix(0, 5, 5) == [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 
 def test_initial_matrix():
-    assert Game_of_life.intial_matrix(Game_of_life.matrix(0, 3, 3)) == [[0, 0, 0],[1, 1, 1], [0, 0, o]]
+    assert Game_of_life.initial_matrix(1, [[0, 0, 0],[1, 1, 1], [0, 0, 0]]
+) == [[0, 0, 0],[1, 1, 1], [0, 0, 0]]
 
 
     
     
-                                                                        ,
+    
