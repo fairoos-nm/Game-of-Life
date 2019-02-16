51ac53353d208d03c2728964c6c5d8e862227369
fairoos <fairoosnm123@gmail.com>
2019-02-16 18:22:16 +0530

    RED: Added test_initial_matrix

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -4,5 +4,11 @@ import Game_of_life
 
 def test_matrix():
     assert Game_of_life.matrix(0, 5, 5) == [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+
+def test_initial_matrix():
+    assert Game_of_life.intial_matrix(Game_of_life.matrix(0, 3, 3)) == [[0, 0, 0],[1, 1, 1], [0, 0, o]]
+
+
     
     
+                                                                        ,
