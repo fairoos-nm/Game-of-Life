3761978b14c4b01731dd11252824ce5532d376db
fairoos <fairoosnm123@gmail.com>
2019-02-20 13:58:05 +0530

    GREEN: Added function for applying four rule of game of life up on given input matrix and provide changed matrix.
    Freeze functions matrix and initial_matrix by make it as comment for troubleshooting some problems.
    
    Removed functions position_of_alive_elements ,apply_rules and main from previous commit  because some ones are useless and some ones are added by mistake

--- Game_of_life.py
+++ Game_of_life.py
@@ -1,42 +1,24 @@
-#Game_of_life
+ # #Game_of_life
 
-def matrix(element, row, column):
-    matrix_out = [[element] * row for i in range(column)]
-    return matrix_out
+# row = 3
+# column = 3
+# element = 0
+# def matrix(element, row, column):
+#     matrix_out = [[element] * row for i in range(column)]
+#     return matrix_out
 
-#a =1
-#matrix1 = [[0, 0, 0],[1, 1, 1], [0, 0, 0]]
+# first_matrix = (matrix(element, row, column))
 
-def initial_matrix(a, matrix1):
-    for sub_list in matrix1:
-        if matrix1[1] == sub_list:
-            for i in sub_list:
-                sub_list[i] = 1
-                return matrix1
+# print (first_matrix)
 
+# def initial_matrix(position, matrix1):
+#     for row_no in range(len(matrix1)):
+#         if row_no == 1:
+#             matrix1[1] = [1, 1, 1]
+#     return matrix1
 
-def neighbours( matrix1, position):
-    for sub_list in matrix1:
-        for j in range(len(matrix1)):
-            for i in range(len(sub_list)):
-                my_pos = [j, i]
-                if my_pos == position:
-                     neighbours_list = [[j-1, i-1], [j-1, i], [j-1, i+1], [j, i-1], [j, j+1],[j+1, i-1],[j+1, i],[j+1, i+1]]
-                     return neighbours_list
-
-def real_neighbours(neighbours_list):
-    a = []
-    for neighbor in neighbours_list:
-        for x in neighbor:
-            if x < 0 or x > 2:
-                neighbor.remove(x)
-        a.append(neighbor)
-        for i in a:
-            if len(i) == 1:
-                a.remove(i)
-    return a
-    
-     
+# initial_stage = initial_matrix(first_matrix)
+# print(initial_stage)
 
 def position_of_alive_elements(initial_matrix):
     alive = 1
@@ -49,33 +31,95 @@ def position_of_alive_elements(initial_matrix):
                 position.append([j, i])
             i = i + 1
         j = j + 1
-    return position                  
-
-def apply_rules(initial_stage, positions):
-    alive = 1
-    dead = 0
-    posi = []
-    for first_position in positions:
-        for j in range(len(initial_stage)):
-            for i in range(len(row)):
-                print [j, i]
-                if [j, i] == first_posi:
-                   for row in initial_stage:
-                       
-        
-    
-    
-    
+    return position 
 
 
-def main():
-    row = 3
-    column = 3
-    element = 0
-    first_matrix = matrix(element, row, column)
-    a = 1
-    initial_stage = initial_matrix(a, first_matrix)
-    positions = position_of_alive_elements(initial_stage)
-    all_neighbors = neighbours( initial_stage, position)
-    neighbors = real_neighbours(all_neighbors)
+def neighbours( matrix1, position):
+    for sub_list in matrix1:
+        for j in range(len(matrix1)):
+            for i in range(len(sub_list)):
+                my_pos = [j, i]
+                if my_pos == position:
+                    neighbours_list = [[j-1, i-1], [j-1, i], [j-1, i+1], [j, i-1], [j, i+1],[j+1, i-1],[j+1, i],[j+1, i+1]]
+                    
+                    return  neighbours_list
+
+
+                 
+def real_neighbours(neighbours_list):
+    a = []
+    for neighbour in neighbours_list:
+        # print (neighbour)
+        for x in neighbour:
+            if x < 0 or x > 2:
+                neighbour.remove(x)
+        a.append(neighbour)
+        for i in a:
+            if len(i) == 1:
+                a.remove(i)
+    return a
+
+
+
+def count_alive_neighbours(metrix):
     
+    rows = len(metrix)
+    cols = len(metrix[0])
+    result = [[None for i in range(cols)] for j in range(rows)]
+    
+    for i in range(len(metrix)):
+        for j in range(len(metrix[0])):
+            position = [i,j]
+            count = 0
+            neib = real_neighbours(neighbours(metrix, position))
+            for a in neib:
+                x = a[0]
+                y = a[1]
+                #print (a, x, y)
+                f = metrix[x]
+                #print(f)
+                n = f[y]
+                #print(n)
+                if n  == 1:
+                    count = count + 1        
+            result[i][j] = count
+    return result
+
+
+
+
+
+metrix = [[0, 0, 0], [1, 1, 1], [0, 0 , 0]]
+
+alive_list = count_alive_neighbours(metrix)
+
+def apply_rules(alive_list, metrix):
+    
+    rows = len(metrix)
+    columns = len(metrix[0])    
+    for i in range(rows):
+        for j in range(columns):
+            if metrix[i][j]:
+                if alive_list[i][j] < 2 or  alive_list[i][j] > 3:
+                    metrix[i][j] = 0
+            if metrix[i][j]:
+                if alive_list[i][j] == 3 or alive_list[i][j] == 2:
+                    metrix[i][j] = 1
+
+            if metrix[i][j] == 0 and alive_list[i][j] == 3:
+                metrix[i][j] =1
+    return metrix
+                
+    
+print(apply_rules(alive_list,metrix))    
+    
+
+# def main():
+
+    
+    
+#     print
+#     positions = position_of_alive_elements(initial_stage)
+#     all_neighbors = neighbours( initial_stage, position)
+#     neighbors = real_neighbours(all_neighbors)
+#     print(neighbours)
