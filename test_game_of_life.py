427b7c81989ec94c09434b2f6c75a971035eed84
fairoos <fairoosnm123@gmail.com>
2019-02-20 13:35:48 +0530

    RED: Added test function for apply rules.

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -25,3 +25,8 @@ def test_position_of_alive_elements():
 
 def test_count_alive_neighbours():
     assert Game_of_life.count_alive_neighbours([[0, 0, 0], [1, 1, 1], [0, 0 , 0]]) == [[2,3,2], [1,2,1], [2,3,2]]
+
+def test_apply_rule():
+    alive_list = [[2,3,2], [1,2,1], [2,3,2]]
+    metrix = [[0, 0, 0], [1, 1, 1], [0, 0 , 0]]
+    assert Game_of_life.apply_rules(alive_list, metrix) == [[0, 1, 0], [0, 1, 0], [0, 1, 0]] 
