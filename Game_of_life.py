74520b366c4ac06c7eb03ed000bf045f234e3ab1
fairoos <fairoosnm123@gmail.com>
2019-02-25 08:18:52 +0530

    Refactor: Removed trailing whitespace Renamed short Variable names like 'a' 'x'

--- Game_of_life.py
+++ Game_of_life.py
@@ -2,53 +2,44 @@
 
 
 def neighbours_of_position(matrix, position):
-
     """ for providing neighbours of a particular position and return all the eight neighbours of that position """
-    
     for sub_list in matrix:
         for j in range(len(matrix)):
             for i in range(len(sub_list)):
                 my_pos = [j, i]
                 if my_pos == position:
-                    neighbours_list = [[j-1, i-1], [j-1, i], [j-1, i+1], [j, i-1], [j, i+1],[j+1, i-1],[j+1, i],[j+1, i+1]]
-                    
+                    neighbours_list = [[j-1, i-1], [j-1, i], [j-1, i+1], [j, i-1], [j, i+1], [j+1, i-1], [j+1, i], [j+1, i+1]]
                     return  neighbours_list
 
 def neighbours(matrix):
     """ provides the eight neighbours of all positions of matrix """
-    
     for sub_list in matrix:
         for j in range(len(matrix)):
-            for i in range(len(sub_list)):
-                my_pos = [j, i]
-                neighbours_list = [[j-1, i-1], [j-1, i], [j-1, i+1], [j, i-1], [j, i+1],[j+1, i-1],[j+1, i],[j+1, i+1]]
-                    
+            for i in range(len(sub_list)): 
+                neighbours_list = [[j-1, i-1], [j-1, i], [j-1, i+1], [j, i-1], [j, i+1], [j+1, i-1], [j+1, i], [j+1, i+1]]
                 return  neighbours_list
 
 def real_neighbours(neighbours_list):
     """provide onboard neighbours of all position form total neighbours """
-    
-    a = []
+    real_neib_list = []
     for neighbour in neighbours_list:
-        for x in neighbour:
-            if x < 0 or x > 2:
-                neighbour.remove(x)
-        a.append(neighbour)
-        for i in a:
-            if len(i) == 1:
-                a.remove(i)
-    return a
+        for elements in neighbour:
+            if elements < 0 or elements > 2:
+                neighbour.remove(elements)
+        real_neib_list.append(neighbour)
+        for posi_list in real_neib_list:
+            if len(posi_list) == 1:
+                real_neib_list.remove(posi_list)
+    return real_neib_list
 
 def count_alive_neighbours(matrix):
     """count number of living neighbours (1) of each position in a matrix """
-    
     rows = len(matrix)
     cols = len(matrix[0])
     result = [[None for i in range(cols)] for j in range(rows)]
-    
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            position = [i,j]
+            position = [i, j]
             count = 0
             neib = real_neighbours(neighbours_of_position(matrix, position))
             for a in neib:
@@ -56,7 +47,7 @@ def count_alive_neighbours(matrix):
                 column_position = a[1]
                 row = matrix[row_position]
                 element = row[column_position]
-                if element  == 1:
+                if element == 1:
                     count = count + 1        
             result[i][j] = count
     return result
@@ -77,22 +68,22 @@ def apply_rules(alive_list, matrix):
                     matrix[i][j] = 1
             """fourth rule"""
             if matrix[i][j] == 0 and alive_list[i][j] == 3:
-                matrix[i][j] =1
+                matrix[i][j] = 1
     return matrix
                 
 def display(board):
     """display the game output in a combination of zeros and period"""
     row = len(board)
-    column =  len(board[0])
-    a = ""
+    column = len(board[0])
+    showcase = ""
     for i in range(row):
         for j in range(column):
             if j == 0:
-                a += "\n"
+                showcase += "\n"
             if board[i][j] == 1:
-                a += "0 "
+                showcase += "0 "
             if board[i][j] == 0:
-                a += ". "
+                showcase += ". "
             
-    return a
+    return showcase
     
